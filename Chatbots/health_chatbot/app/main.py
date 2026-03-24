8030231863ba22c02d599e183125ae247d6d7717
Rijul Kumar <93815856+Rijulkumar26@users.noreply.github.com>
2026-03-24 23:44:10 +0530

    Update main.py

--- Chatbots/health_chatbot/app/main.py
+++ Chatbots/health_chatbot/app/main.py
@@ -4,11 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "API is running"}
-
-# Import AFTER app is created
-try:
-    from app.api.routes import router
-    app.include_router(router)
-except Exception as e:
-    print("ERROR DURING STARTUP:", e)
+    return {"message": "Server is working"}
