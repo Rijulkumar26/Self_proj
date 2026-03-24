477cd1dd397ed859734d730247290cfe6923cd09
Rijul Kumar <93815856+Rijulkumar26@users.noreply.github.com>
2026-03-24 23:58:18 +0530

    Update main.py

--- Chatbots/health_chatbot/app/main.py
+++ Chatbots/health_chatbot/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.api.routes import router
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Server is working"}
+    return {"message": "UHC Chatbot API is running"}
+
+# Import router ONLY
+app.include_router(router)
